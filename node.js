54bbd01d338733a90242c38ac892f88d2b561fd9
Muhammed Fahiz k <fahizk100@gmail.com>
2023-11-20 23:03:59 -0500

    read user data

--- node.js
+++ node.js
@@ -1,46 +1,102 @@
-var fs = require('fs');
 var http = require('http');
+var fs = require('fs');
 var path = require('path');
-var url = require('url')
-var mainpage=fs.readFileSync('./index.html','utf-8')
-var page =fs.readFileSync('userdata.html','utf-8')
-let userdata =JSON.parse(fs.readFileSync('Data/user.json','utf-8') )
+var url = require('url');
 
-    let dataarray =userdata.map((value)=>{
-        let output = page.replace('{{%name}}',value.name);
-        output=output.replace('{{%age}}',value.age)
-        output=output.replace('{{%email}}',value.email)
-        output=output.replace('{{%number}}',value.number)
-        return output;
-    })
-        http.createServer((req, res) => {
+http.createServer((req, res) => {
+    try {
+        const pathname = url.parse(req.url).pathname;
 
-            if (req.url === '/') {
-                try {
-                    var data = fs.readFileSync('index.html');
-                    let responseprodect =mainpage.replace('{{%content}}',dataarray.join(','))
-
-                    res.writeHead(200, { 'content-type': 'text/html' });
-                    res.write(responseprodect);
-                    res.end();
-                } catch (err) {
-                    res.writeHead(500, { 'content-type': 'text/plain' });
-                    res.end("Inner function error 1");
-                    console.log(err)
-                }
-
-                    } else if (req.url === '/form.html') {
-                      
-                try {
-                    var data = fs.readFileSync('form.html');
-                    res.writeHead(200, { 'content-type': 'text/html' });
+        if (pathname === '/') {
+            fs.readFile('index.html', 'utf-8', (err, data) => {
+                if (err) {
+                    res.writeHead(500, {'content-type': 'text/plain'});
+                    res.end('Error reading the file');
+                } else {
+                    res.writeHead(200, {'content-type': 'text/html'});
                     res.write(data);
                     res.end();
-                    let x = url.parse(req.url,true)
-                    console.log(x)
-                } catch (err) {
-                    res.writeHead(500, { 'content-type': 'text/plain' });
-                    res.end("Inner function error 1");
                 }
-                    }
-        }).listen(8000);
+            });
+        }else if(pathname ==='/form.html'){
+            fs.readFile('form.html','utf-8',(err,data)=>{
+                if(err){
+                    res.writeHead(500,{'content-type':'text/plain'})
+                    res.end('error reading file form page')
+                    console.log(err)
+                }
+                else{
+                    res.writeHead(200,{'content-type':'text/html'})
+                    res.write(data)
+
+                    res.end()
+
+                    
+                }
+            })
+
+        }else if(pathname === '/myform' && req.method === 'POST'){
+            let body = ''
+            req.on('data', (chunk)=>{
+                body+=chunk;
+            })
+            req.on('end',()=>{
+                const formData = new URLSearchParams(body);
+                const name = formData.get('name');
+                const age = formData.get('age');
+                const number = formData.get('number');
+                const email = formData.get('email');
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                fs.readFile('Data/data.html','utf-8',(err,datajson)=>{
+                     
+                     const formsubmition = {
+                        name: name,
+                        age:age,
+                        number:number,
+                        email:email
+                     }
+                    let existdata=[];
+                     try{
+                    existdata=JSON.parse(data)
+
+                     }catch{
+                        console.error('Error reading data.json:', err);
+
+                     }
+                     existdata.push(formsubmition)
+                     fs.appendFile('Data/data.json', JSON.stringify(existdata, null, 2), 'utf-8', (err,newdata) => {
+                        if (err) {
+                            console.error('Error writing data.json:', err);
+                        } else {
+                            console.log('Data written to data.json successfully');
+                            res.end()
+                        }
+                    });
+    
+                     fs.readFile('data.json','utf-8',(err,data)=>{
+                        if(err){
+                            res.writeHead(500,{'content-type':'text/plain'})
+                            res.end(newdata)
+                        }
+                        else{
+                            res.writeHead(500,{'content-type':'application/json'})
+                            res.end('ok')
+                            console.log(data)
+                        }
+                     })
+                })
+                res.end(`Received form submission: Name - ${name}, Age - ${age}, Number - ${number}, Email - ${email}`);
+           
+            })
+       }else{ {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Not Found');
+              }      
+        }
+
+    } catch (err) {
+        res.writeHead(400, {'content-type': 'text/plain'});
+        console.log(err)
+        res.end('Bad Request');
+    }
+}).listen(8000);
